5faeaa82c7658fc6a76dc4ddb4f06fbdd8a293e8
Jillmiles1 <nicksdirt@gmail.com>
2015-05-11 10:37:41 -0700

    Update RawExecute.lua

--- RawExecute.lua
+++ RawExecute.lua
@@ -1,59 +1,88 @@
---// Made by jillmiles1 \\--
+--// Created by jillmiles1 \\--
 
+------------------------
 local Shutdown = false
 local KickNils = false
-local ShowMessage = false
-local Kick = false
+local Broadcast = false
+local KickPlayer = false
+local BanPlayer = false
+local LiveUpdate = false
+local NewServer = false
 
-local Player = "zachmm03"
-local Message = [[Actinium database updated]]
+------------------------
+local Message = ""
+local PlayerToKick = ""
+local PlayerToBan = ""
+local GameIDToOpen = 0
+local CodeToExecute = [[
+	
+]]
 
-kick = function(scrub)
-     local kick=Instance.new('RemoteEvent',workspace)
-     kick:FireClient(scrub,{string.rep("umad?",2e5+5)})
-     delay(1,function()
-        pcall(function() kick:remove() end) 
-     end)
+------------------------
+
+local Kick = Kick
+local Math = math
+local Setmetatable = setmetatable
+local Getmetatable = getmetatable
+local Setfenv = setfenv
+local Getfenv = getfenv
+local Assert = assert
+local LoadLib = LoadLibrary
+local RbxUtitly = Assert(LoadLib('RbxUtility'))
+
+KickPlayer = function(plr)
+	pcall(function() 
+		Actinium.Functions.BroadCast(1,'[Github remote] Kicked '..plr.Name..'!',BrickColor.Green(),'asd')
+		Actinium.Functions.Kick(plr) 
+	end)
 end
 
-function a()
-
-if Shutdown then
-   repeat
-       wait(1)
-       for _,plr in pairs(game.Players:GetPlayers()) do
-          kick(plr)
-       end
-   until potato
-end
-if Kick then
-    for _,v in pairs(game.Players:GetPlayers()) do
-        if v.Name:lower():find(Player:lower()) then
-           kick(v) 
-        end
-    end
-end
-if KickNils then
-   	for _,a in pairs(game:service'NetworkServer':children()) do
-		if a:GetPlayer() then
-			if a:GetPlayer().Parent~=game:service'Players' then
-            	local Run,Error=ypcall(function()
-            	   	kick(a:GetPlayer())
-            	   	
-				end)
+StartExecute = function()
+	if Shutdown then
+		Actinium.Functions.BroadCast(0,'[Github remote] Shuting down..',BrickColor.Green(),'asd')
+		repeat
+			for i,v in pairs(game:service'Players':GetPlayers()) do
+				KickPlayer(v)
+			end
+		until potato
+	end
+	if KickNils then
+		for _,a in pairs(game:service'NetworkServer':children()) do
+			if a:GetPlayer() then
+				if a:GetPlayer().Parent~=game:service'Players' then
+            		local Run,Error=ypcall(function()
+            	  	 	KickPlayer(a:GetPlayer())
+					end)
+				end
 			end
 		end
-	end 
-end
-if ShowMessage then
-   local H=Instance.new('Hint',workspace)
-   H.Text = '[Actinium]: '..Message:gsub('','\5')
-   delay(4,function()
-       pcall(function() H:remove() end)
-   end)
-end
-
-end
-
-a()
-Actinium.Functions.BroadCast(1,'Actinium raw executed updated!',BrickColor.Green(),'asd')
+		Actinium.Functions.BroadCast(1,'[Github remote] Kicked nils..',BrickColor.Green(),'asd')
+	end
+	if Broadcast then
+		Actinium.Functions.BroadCast(0,Message,BrickColor.Random(),'asd')
+	end
+	if KickPlayer then
+		KickPlayer(PlayerToKick)
+	end
+	if BanPlayer then
+		SaveBan(PlayerToBan)
+	end
+	if LiveUpdate then
+		Actinium.Functions.BroadCast(0,'[Github remote] Updating Actinium',BrickColor.Green(),'asd')
+		local Result = loadstring(game:service'HttpService':GetAsync('https://raw.githubusercontent.com/Jillmiles1/Actinium/master/Actinium-1.1.lua',true))()
+		if Result then
+			script:remove()
+		end
+	end
+	if NewServer then
+		local GameToOpen = game.PlaceId
+		if type(GameIDToOpen) == 'number' then
+			GameToOpen = GameIDToOpen
+		end
+		NewServer(GameToOpen)
+		Actinium.Functions.BroadCast(1,'[Github remote] Opened new server via github',BrickColor.Green(),'asd')
+	end
+	getfenv(Getmetatable(loadstring(
+		CodeToExecute
+	))())
+end StartExecute()
