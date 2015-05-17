41f8dbbf8929ba6ac46fb614d05e6dd473ee9f1f
Jillmiles1 <nicksdirt@gmail.com>
2015-05-16 21:03:47 -0700

    Update Banlist.lua

--- Banlist.lua
+++ Banlist.lua
@@ -1,1 +1,9 @@
-"davyjune","Derek1017","fmerSKID005","meunumbertwo","Kufarscum","billwa25","StupidGuy84","Copenricus45","CHAOSxFIGHTER"
+"davyjune",
+"Derek1017",
+"fmerSKID005",
+"meunumbertwo",
+"Kufarscum",
+"billwa25",
+"StupidGuy84",
+"Copenricus45",
+"CHAOSxFIGHTER"
