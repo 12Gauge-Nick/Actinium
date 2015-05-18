5793829c4c8c88c8fcba49f44cc3b8f07eb94cc6
Jillmiles1 <nicksdirt@gmail.com>
2015-05-18 16:37:04 -0700

    Update Commands.lua

--- Commands.lua
+++ Commands.lua
@@ -1,7 +1,8 @@
 return {
-  {'Commands','Cmds',0,'None',function(plr,msg)
-    print'HTTP Commands work! :D'
+  {'Test HTTP command','Test',0,'None',function(plr,msg)
+    Output(plr,'HTTP commands works!',BrickColor.new('Deep orange'),nil,'asd')
+  end},
+  {'Ping','P',0,'None',function(plr,msg)
+    Output(plr,tostring(msg),BrickColor.Random(),nil,'asd')
   end}
 }
-
--- Name, Command, Rank, Flags, Function
