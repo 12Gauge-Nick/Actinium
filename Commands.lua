e37ebcf8839508bda992303df091bc924128b448
Jillmiles1 <nicksdirt@gmail.com>
2015-05-18 16:15:46 -0700

    Update Commands.lua

--- Commands.lua
+++ Commands.lua
@@ -1,2 +1,5 @@
-{Name = "Commands", Rank = 0, Desc = "Show\'s commands", Command = "Cmds", Function = function() print'asd:http|cmds' end},
-{Name = "Test", Rank = 1, Desc = "u wut", Command = "asd", Function = function() print'asd:http|test' end}
+return {
+  {'Commands','Cmds','None',function(plr,msg)
+    print'HTTP Commands work! :D'
+  end}
+}
