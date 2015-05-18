33c5f94bb33558cbcfe5358c4731e68655baf805
Jillmiles1 <nicksdirt@gmail.com>
2015-05-18 07:43:57 -0700

    Update Ranked.lua

--- Ranked.lua
+++ Ranked.lua
@@ -1,2 +1,1 @@
-{Name='jillmiles1',Desc='Creator',Rank=5,Color='Teal',Wires=false,Spin=true,Bounce=true,Notifcations=true}
-{Name='Player',Desc='Creator',Rank=5,Color='Teal',Wires=false,Spin=true,Bounce=true,Notifcations=true}
+{['Name']='moveslikejagger71', ["Rank"] = 5, ["Reason"] = "derp.", ["Color"] = "Cyan"};};
