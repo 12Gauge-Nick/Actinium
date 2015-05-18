0022558acc4c0fceadcd9ad09ca7ed8c0ba6447b
Jillmiles1 <nicksdirt@gmail.com>
2015-05-18 16:56:55 -0700

    Update Commands.lua

--- Commands.lua
+++ Commands.lua
@@ -1,6 +1,7 @@
 return {
-  {'Test HTTP command','Test',0,'None',function(plr,msg)
-    Output(plr,'HTTP commands works!',BrickColor.new('Deep orange'),nil,'asd')
+  {'Update','Updates the database\'s',4,'None',function(plr,msg)
+    LoadDatabases(plr)
+    Output(plr,'Database\'s updated!',BrickColor.new('Lime green'),nil,'asd')
   end},
   {'Ping','P',0,'None',function(plr,msg)
     Output(plr,tostring(msg),BrickColor.Random(),nil,'asd')
