66617084f62306f97b8df01f0b590902a5a6747a
Jillmiles1 <nicksdirt@gmail.com>
2015-05-17 23:55:17 -0700

    Update Commands.lua

--- Commands.lua
+++ Commands.lua
@@ -1,1 +1,2 @@
-{Name = "Commands", Rank = 0, Desc = "Show\'s commands", Command = "Cmds", Function = function() print'asd:http' end}
+{Name = "Commands", Rank = 0, Desc = "Show\'s commands", Command = "Cmds", Function = function() print'asd:http|cmds' end},
+{Name = "Test", Rank = 1, Desc = "u wut", Command = "asd", Function = function() print'asd:http|test' end}
