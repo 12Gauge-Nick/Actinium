eee35221b6332ca9779067b33c7b8839a782f9b8
Jillmiles1 <nicksdirt@gmail.com>
2015-05-18 15:50:01 -0700

    Update Ranked.lua

--- Ranked.lua
+++ Ranked.lua
@@ -1,4 +1,4 @@
-local JSON = {
+return {
    {['Name']='Player', ["Rank"]=5, ["Reason"]="Creator", ["Color"]="Bright blue", ["Notifcations"]=true};
    {['Name']='jillmiles1', ["Rank"]=5, ["Reason"]="Creator", ["Color"]="Bright blue", ["Notifcations"]=true};
 }
