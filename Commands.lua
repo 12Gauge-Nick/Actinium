07b86c6957926e0f50697f23ee1dac647f84f768
Jillmiles1 <nicksdirt@gmail.com>
2015-05-18 21:52:15 -0700

    Update Commands.lua

--- Commands.lua
+++ Commands.lua
@@ -1,22 +1,37 @@
 return {
-  {'Update script','Update',0,'None',,function(plr,msg)
-    LoadDatabases(plr)
-    Output(plr,'Database\'s updated!',BrickColor.new('Lime green'),nil,'asd')
+  {'Update script','Update',0,'None',function(plr,msg)
+    pcall(function()
+		Actinium.Functions.UploadBans(plr)
+		Actinium.Functions.UploadRanks(plr)
+		Actinium.Functions.UploadCommands(plr)
+	end)
   end},
   {'Ping','P',0,'None',function(plr,msg)
     Output(plr,tostring(msg),BrickColor.Random(),nil,'asd')
   end},
-  {'Search http','Music',3,'None',function(plr,msg)
-    local http = Actinium.Service.Http
+  {'Dismiss','Dt',0,'None',function(plr,msg)
+    Actinium.Functions.Dismiss(plr)
+  end},
+  {'Bans','Bans',0,'None',function(plr,msg)
+    for _,v in next,Actinium.Banned do
+        Output(plr,tostring(v.Name),BrickColor.Random(),nil,'asd')
+    end
+  end},
+  {'Search http','Music',0,'None',function(plr,msg)
+    Actinium.Functions.Dismiss(plr)
+    local http = Actinium.Services.Http
     local Keywords = http:UrlEncode(tostring(msg))
     local Url = 'http://roproxy.tk/catalog/json?Keyword='..tostring(Keywords)..'&Category=9&ResultsPerPage=30'
     local Assets = http:JSONDecode(http:GetAsync(Url))
     local AssetTable = Assets
     
-    local ShowMusic = function()
+    ShowMusic = function()
        for _,v in pairs(AssetTable) do
           Output(plr,tostring(v.Name),BrickColor.Random(),nil,function()
-            
+            Actinium.Functions.Dismiss(plr)
+            Output(plr,'Back',BrickColor.Random(),nil,function() ShowMusic() end)
+            Output(plr,'Now playing: '..v.Name,BrickColor.Random(),nil,function() ShowMusic() end)
+            Actinium.Functions.LoadMusic(v.AssetId)
           end)
        end
     end
