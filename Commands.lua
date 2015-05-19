45b337def2f502b32e05aaf1cb23da6e8b7631b0
Jillmiles1 <nicksdirt@gmail.com>
2015-05-18 17:14:43 -0700

    Update Commands.lua

--- Commands.lua
+++ Commands.lua
@@ -1,12 +1,12 @@
 return {
-  {'Update script','Update','Updates the database\'s',4,'None',function(plr,msg)
+  {'Update script','Update',0,'None',,function(plr,msg)
     LoadDatabases(plr)
     Output(plr,'Database\'s updated!',BrickColor.new('Lime green'),nil,'asd')
   end},
   {'Ping','P',0,'None',function(plr,msg)
     Output(plr,tostring(msg),BrickColor.Random(),nil,'asd')
   end},
-  {'Search Music','Music','Updates the database\'s',4,'None',function(plr,msg)
+  {'Search http','Music',3,'None',function(plr,msg)
     local http = Actinium.Service.Http
     local Keywords = http:UrlEncode(tostring(msg))
     local Url = 'http://roproxy.tk/catalog/json?Keyword='..tostring(Keywords)..'&Category=9&ResultsPerPage=30'
