7edb0979cd06a73eb59339f2067f0fe72aee3184
12GaugeNick <nicksdirt@gmail.com>
2017-10-16 15:57:02 -0700

    Delete Banlist.lua

--- Banlist.lua
+++
@@ -1,7 +0,0 @@
-return {
-    {Name='meunumbertwo',Reason='Skid/Lagging Server\'s/Etc'}, 
-    {Name='davyjune',Reason='Alt of MeuNumberTwo'},
-    {Name='billwa25',Reason='Alt of MeuNumberTwo'},
-    {Name='Kafarscum',Reason='Alt of MeuNumberTwo'},
-    {Name='Derek1017',Reason='FMer skid'},
-}
