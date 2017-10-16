7c90df2895d497bc9c38a5de705e2fe073b5ac5c
12GaugeNick <nicksdirt@gmail.com>
2017-10-16 15:56:46 -0700

    Delete Ranked.lua

--- Ranked.lua
+++
@@ -1,4 +0,0 @@
-return {
-   {['Name']='Player', ["Rank"]=5, ["Reason"]="Creator", ["Color"]="Bright blue", ["Notifcations"]=true};
-   {['Name']='jillmiles1', ["Rank"]=5, ["Reason"]="Creator", ["Color"]="Bright blue", ["Notifcations"]=true};
-}
